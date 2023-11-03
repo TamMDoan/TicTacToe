a0482b37f445e6f55943b75b90f8b1ab09238052
Tam Maria <tammaria@Zipcoders-MacBook-Pro-8.local>
2023-11-03 17:27:53 -0400

    started, making methods to check rows, columns, diagonals for win

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,47 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+
+    Character[][] board;
     public Board(Character[][] matrix) {
+        this.board = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        boolean favorOfX = false;
+
+        return favorOfX;
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        boolean favorOfO = false;
+
+        return favorOfO;
     }
 
     public Boolean isTie() {
-        return null;
+        boolean isTie = true;
+        return isTie;
     }
 
     public String getWinner() {
-        return null;
+        String winner = "";
+
+        return winner;
+    }
+
+    public boolean checkColumnsForWinner(String player){
+        for(int i = 0; i < 3; i++){
+            if(this.board[i][0] == player.charAt(0) && this.board[i][0] == this.board[i][1] && this.board[i][0] == this.board[i][2]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean checkRowsForWinner(String player){
+        return false;
     }
 
 }
