6e483798d8b4b87e3da603b1b47bad450cf3a1ab
Tam Maria <tammaria@Zipcoders-MacBook-Pro-8.local>
2023-11-03 17:54:22 -0400

    finished, tests passing

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -11,29 +11,31 @@ public class Board {
     }
 
     public Boolean isInFavorOfX() {
-        boolean favorOfX = false;
 
-        return favorOfX;
+        return checkColumnsForWinner("X") || checkRowsForWinner("X") || checkDiagonalsForWinner("X");
     }
 
     public Boolean isInFavorOfO() {
-        boolean favorOfO = false;
 
-        return favorOfO;
+        return checkColumnsForWinner("O") || checkRowsForWinner("O") || checkDiagonalsForWinner("O");
     }
 
     public Boolean isTie() {
-        boolean isTie = true;
-        return isTie;
+
+        return isInFavorOfX() == isInFavorOfO();
     }
 
     public String getWinner() {
-        String winner = "";
-
-        return winner;
+        if(isInFavorOfO()){
+            return "O";
+        }
+        else if(isInFavorOfX()){
+            return "X";
+        }
+        return "";
     }
 
-    public boolean checkColumnsForWinner(String player){
+    public boolean checkRowsForWinner(String player){
         for(int i = 0; i < 3; i++){
             if(this.board[i][0] == player.charAt(0) && this.board[i][0] == this.board[i][1] && this.board[i][0] == this.board[i][2]) {
                 return true;
@@ -43,7 +45,20 @@ public class Board {
         return false;
     }
 
-    public boolean checkRowsForWinner(String player){
+    public boolean checkColumnsForWinner(String player){
+        for(int i = 0; i < 3; i++){
+            if(this.board[0][i] == player.charAt(0) && this.board[0][i] == this.board[1][i] && this.board[2][i] == this.board[0][i]){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean checkDiagonalsForWinner(String player){
+        if(this.board[1][1] == player.charAt(0)){
+            return (this.board[0][0] == this.board[2][2] && this.board[0][0] == player.charAt(0)) ||
+                    (this.board[0][2] == this.board[2][0] && this.board[0][2] == player.charAt(0));
+        }
         return false;
     }
 
